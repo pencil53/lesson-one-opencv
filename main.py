3b387fc31955aad0c679daf4ea08d8f154d99025
pencil53 <118580500+pencil53@users.noreply.github.com>
2024-02-06 22:04:27 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,18 @@
+#creating variable
+name = "Akshay"
+age = 15
+#printing data
+print ("You are",age, "years old")
+print ("You are {} years old".format(age))
+print ("My name is {}. I am {} years old.".format(name, age))
+
+#revising list
+
+marks = [64,100,102,94,89]
+print (marks)
+print (marks[2])
+#adding into the lis
+marks.append(96)
+print(marks)
+
+#2d list
